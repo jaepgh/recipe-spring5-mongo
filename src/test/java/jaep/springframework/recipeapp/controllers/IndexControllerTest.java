0029e409faf5f7d286830aed54e0501eb7d2a263
jaepgh <jaesquivel2007@gmail.com>
2019-02-03 10:54:37 -0500

    Integration test implemented. Closes #10.

--- src/test/java/jaep/springframework/recipeapp/controllers/IndexControllerTest.java
+++ src/test/java/jaep/springframework/recipeapp/controllers/IndexControllerTest.java
@@ -1,17 +1,26 @@
 package jaep.springframework.recipeapp.controllers;
 
+import jaep.springframework.recipeapp.domain.Recipe;
 import jaep.springframework.recipeapp.services.RecipeService;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MockMvcBuilder;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.ui.Model;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import static org.junit.Assert.*;
-import static org.mockito.ArgumentMatchers.anySet;
 import static org.mockito.ArgumentMatchers.eq;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
 public class IndexControllerTest {
     @Mock
@@ -30,9 +39,32 @@ public class IndexControllerTest {
 
     @Test
     public void getIndexPage() {
+        Set<Recipe> recipes = new HashSet<>();
+        recipes.add(new Recipe());
+
+        Recipe aux = new Recipe();
+        aux.setId(5L);
+        recipes.add(aux);
+
+        when(recipeService.getAllRecipes()).thenReturn(recipes);
+
+        ArgumentCaptor<Set<Recipe>> argumentCaptor = ArgumentCaptor.forClass(Set.class);
 
         assertEquals("index", indexController.getIndexPage(model));
         verify(recipeService, times(1)).getAllRecipes();
-        verify(model, times(1)).addAttribute(eq("recipes"), anySet());
+        verify(model, times(1)).addAttribute(eq("recipes"), argumentCaptor.capture());
+
+        Set<Recipe> setInController = argumentCaptor.getValue();
+        assertEquals(2, setInController.size());
+
+    }
+
+    @Test
+    public void testMockMVC() throws Exception {
+        MockMvc mockMvc = MockMvcBuilders.standaloneSetup(indexController).build();
+        mockMvc.perform(get("/"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("index"));
+
     }
 }
---
+++ src/test/java/jaep/springframework/recipeapp/repositories/UnitOfMeasureRepositoryTestIT.java
@@ -0,0 +1,38 @@
+package jaep.springframework.recipeapp.repositories;
+
+import jaep.springframework.recipeapp.domain.UnitOfMeasure;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Optional;
+
+import static org.junit.Assert.*;
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+public class UnitOfMeasureRepositoryTestIT {
+
+    @Autowired
+    UnitOfMeasureRepository unitOfMeasureRepository;
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void findByDescriptionCup() throws Exception {
+        Optional<UnitOfMeasure>  unitOfMeasure = unitOfMeasureRepository.findByUom("Cup");
+
+        assertEquals("Cup", unitOfMeasure.get().getUom());
+    }
+    @Test
+    public void findByDescription() throws Exception {
+        Optional<UnitOfMeasure>  unitOfMeasure = unitOfMeasureRepository.findByUom("Teaspoon");
+
+        assertEquals("Teaspoon", unitOfMeasure.get().getUom());
+    }
+}
