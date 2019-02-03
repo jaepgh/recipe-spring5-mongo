0bbf26e7c1f0baa32f39624b567b55cd04c036af
jaepgh <jaesquivel2007@gmail.com>
2019-02-02 22:48:28 -0500

    Index Controller class tested. Closes #9.

---
+++ src/test/java/jaep/springframework/recipeapp/controllers/IndexControllerTest.java
@@ -0,0 +1,38 @@
+package jaep.springframework.recipeapp.controllers;
+
+import jaep.springframework.recipeapp.services.RecipeService;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.ui.Model;
+
+import static org.junit.Assert.*;
+import static org.mockito.ArgumentMatchers.anySet;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+public class IndexControllerTest {
+    @Mock
+    RecipeService recipeService;
+
+    @Mock
+    Model model;
+
+    IndexController indexController;
+
+    @Before
+    public void setUp() throws Exception {
+        MockitoAnnotations.initMocks(this);
+        indexController = new IndexController(recipeService);
+    }
+
+    @Test
+    public void getIndexPage() {
+
+        assertEquals("index", indexController.getIndexPage(model));
+        verify(recipeService, times(1)).getAllRecipes();
+        verify(model, times(1)).addAttribute(eq("recipes"), anySet());
+    }
+}
